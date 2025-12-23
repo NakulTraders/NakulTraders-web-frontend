0713c98e3874b7195401e5e5aa05991d4476d67f
krishnaVerma1302 <17krishnaverma@gmail.com>
2025-12-23 06:03:25 +0530

    first deploy push

--- src/api/NwConfig.js
+++ src/api/NwConfig.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:7878/"
+export const API_URL = "https://neeraj-pickle-backend.onrender.com/"
+// export const API_URL = "http://localhost:7878/"
 // export const API_URL = "http://localhost:5000/"
 
 
