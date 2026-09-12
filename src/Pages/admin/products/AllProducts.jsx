d88ef1667ff495eb510f7ea0000b29e09b926a2e
krishnaVerma <krishnasverma1302@gmail.com>
2026-09-12 15:06:35 +0530

    currect Category name and update database

--- src/Pages/admin/products/AllProducts.jsx
+++ src/Pages/admin/products/AllProducts.jsx
@@ -185,11 +185,11 @@ export default function AllProducts() {
                             <option value="">All products</option>
                             <option value="achar">Achar</option>
                             <option value="murabba & candy">Murabba & Candy</option>
-                            <option value="papard">Papard</option>
-                            <option value="rice papard">Rice Papard</option>
-                            <option value="fry papard">Fry Papard</option>
-                            <option value="imported fryms">Imported Fryms</option>
-                            <option value="figur fryms">Figur Fryms</option>
+                            <option value="papad">Papad</option>
+                            <option value="rice papad">Rice Papad</option>
+                            <option value="fry papad">Fry Papad</option>
+                            <option value="imported fryums">Imported Fryums</option>
+                            <option value="figure fryums">Figure Fryums</option>
                             <option value="noodles">Noodles</option>
                             <option value="other">Other</option>
                             <option value="seasonal">Seasonal</option>
